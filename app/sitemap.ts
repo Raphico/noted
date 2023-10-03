9b327f021d23ebacb659f2621a2b112e980dee45
Raphico <raphicogit@gmail.com>
2023-10-03 14:42:19 -0700

    add og image, modify sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,30 @@
-import { env } from "@/env.mjs"
+import { absoluteUrl } from "@/lib/utils"
 import { type MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-   const baseUrl = env.NEXT_PUBLIC_APP_URL
-
    return [
       {
-         url: baseUrl,
+         url: absoluteUrl("/"),
          lastModified: new Date(),
       },
       {
-         url: baseUrl + "/login",
+         url: absoluteUrl("/sign-in"),
+         lastModified: new Date(),
+      },
+      {
+         url: absoluteUrl("/sign-up"),
+         lastModified: new Date(),
+      },
+      {
+         url: absoluteUrl("/profile"),
+         lastModified: new Date(),
+      },
+      {
+         url: absoluteUrl("/search"),
+         lastModified: new Date(),
+      },
+      {
+         url: absoluteUrl("/editor"),
          lastModified: new Date(),
       },
    ]
